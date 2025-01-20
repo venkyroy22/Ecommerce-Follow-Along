730b71cc9b9d5cee1fa2ea330017751a0bd27aa9
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-01-20 10:15:44 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const app = express()
-const ErrorHandler = require('./utils/ErrorHandler')
+const express = require("express");
+const app = express();
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-if(process.env.NODE_ENV !== 'PRODUCTION'){
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/",express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
-        path:"Backend/config/.env"
-    })
-}
+      path: "backend/config/.env",
+    });
+};
+//import Routes
+const user = require("./controller/user");
 
-app.use((err,req,res,next)=>{
-    if(err instanceof ErrorHandler){
-        return res.status(err.statusCode || 500).json({
-            message: err.message,
-            stack:err.stack
-        });
-    }
-    res.status(500).json({message:"Internal Server Error"});
 
-})
+app.use("/api/v2/user", user);
 
+
+// it's for ErrorHandling
+app.use(ErrorHandler);
 
 
 module.exports = app;
