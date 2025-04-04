794e54059f97f5dc30edd4af9754eefe27a94a99
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-04-04 14:12:09 +0530

    Done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,15 +1,17 @@
 // react-app/src/pages/Home.js
-
+//milestone_16 completed
 import React, { useEffect, useState } from "react";
 import Product from "../components/auth/Product";
+import { useSelector } from "react-redux"; // Import useSelector to access Redux state
 import Nav from "../components/auth/nav";
-import { Link } from "react-router-dom";
+
 
 export default function Home() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true); // For loading state
   const [error, setError] = useState(null); // For error handling
-
+  const dataRedux = useSelector((state) => state.user);
+  const [data, setdata] = useState();
   useEffect(() => {
     fetch("http://localhost:8000/api/v2/product/get-products")
       .then((res) => {
@@ -44,9 +46,7 @@ export default function Home() {
         <h1 className="text-3xl text-center text-white py-6">Product Gallery</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
           {products.map((product) => (
-            <Link key={product._id} to={`/product-details/${product._id}`}>
-            <Product {...product} />
-          </Link>
+            <Product key={product._id} {...product} />
           ))}
         </div>
       </div>
--- Frontend/src/pages/cart.jsx
+++ Frontend/src/pages/cart.jsx
@@ -2,12 +2,15 @@
 import React, { useState, useEffect } from 'react';
 import CartProduct from '../components/auth/CartProduct';
 import Nav from '../components/auth/nav';
+import { useSelector } from 'react-redux'; // Import useSelector to access Redux state
+
 import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 
 const Cart = () => {
   const [products, setProducts] = useState([]);
   const navigate = useNavigate(); // Initialize navigate
-
+  const data = useSelector((state) => state.user);
+  const token = localStorage.getItem('token');
   useEffect(() => {
     fetch(`http://localhost:8000/api/v2/product/cartproducts?email=${'kanneha@gmail.com'}`)
       .then((res) => {
